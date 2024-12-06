dbdcb9109c18f5267d62aa718c0d180fd0916208
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-12-06 16:08:37 +0100

    Initialisation de projet avec les dépendences

---
+++ src/test/java/ma/nabil/MajesticCup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.nabil.MajesticCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
